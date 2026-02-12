db0bc309f09366b22d8b3e1dc6c0f4c7325fafa1
Dawid <dawsze@alu.edu.gva.es>
2026-02-12 13:36:25 +0100

    Rutas creadas

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\EspeciesController;
+use App\Http\Controllers\EventosController;
+use App\Http\Controllers\UsuariosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('inicio');
+})->name('inicio');
+
+Route::resource('usuarios',UsuariosController::class);
+Route::resource('especies',EspeciesController::class);
+Route::resource('eventos',EventosController::class);
