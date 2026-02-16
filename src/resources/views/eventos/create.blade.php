e105f137aae03d7298614901163081bd12dbec98
jorge_lol20 <jorgejorgemonovar@gmail.com>
2026-02-16 09:25:09 +0100

    Modificar formularios para añadir el old()

--- src/resources/views/eventos/create.blade.php
+++ src/resources/views/eventos/create.blade.php
@@ -2,43 +2,43 @@
     <form action="" method="POST">
         @csrf
         <label for="nombre">Nombre del evento:</label>
-        <input type="text" name="nombre" id=""><br>
+        <input type="text" name="nombre" id="" value="{{ old('nombre') }}"><br>
         @error('nombre')
             <span>{{ $message }}</span>
         @enderror
         <br>
         <label for="tipo_evento">Tipo de evento:</label>
-        <input type="text" name="tipo_evento" id=""><br>
+        <input type="text" name="tipo_evento" id="" value="{{ old('tipo_evento') }}"><br>
         @error('tipo_evento')
             <span>{{ $message }}</span>
         @enderror
         <br>
         <label for="tipo_terreno"></label>
-        <input type="text" name="tipo_terreno" id=""><br>
+        <input type="text" name="tipo_terreno" id="" value="{{ old('tipo_terreno') }}"><br>
         @error('tipo_terreno')
             <span>{{ $message }}</span>
         @enderror
         <br>
         <label for="ubicacion"></label>
-        <input type="text" name="ubicacion" id=""><br>
+        <input type="text" name="ubicacion" id="" value="{{ old('ubicacion') }}"><br>
         @error('ubicacion')
             <span>{{ $message }}</span>
         @enderror
         <br>
         <label for="fecha">Fecha del evento: </label>
-        <input type="date" name="fecha" id=""><br>
+        <input type="date" name="fecha" id="" value="{{ old('fecha') }}"><br>
         @error('fecha')
             <span>{{ $message }}</span>
         @enderror
         <br>
         <label for="descripcion">Descripción del evento: </label>
-        <input type="text" name="descripcion" id=""><br>
+        <input type="text" name="descripcion" id="" value="{{ old('descripcion') }}"><br>
         @error('descripcion')
             <span>{{ $message }}</span>
         @enderror
         <br>
         <label for="imagen">Imagen del evento:</label>
-        <input type="file" name="imagen" id=""><br>
+        <input type="file" name="imagen" id="" value="{{ old('imagen') }}"><br>
         @error('imagen')
             <span>{{ $message }}</span>
         @enderror
--- src/resources/views/usuarios/login.blade.php
+++ src/resources/views/usuarios/login.blade.php
@@ -2,9 +2,9 @@
     <form action="usuarios.login" method="POST">
         @csrf
         <label for="email">Email:</label>
-        <input type="text" name="email"><br>
+        <input type="text" name="email" value="{{ old('email') }}"><br>
         <label for="password">Contraseña:</label>
-        <input type="text" name="password"><br>
+        <input type="text" name="password" value="{{ old('password') }}"><br>
         <input type="submit" value="Iniciar sesión">
     </form>
 </div>
