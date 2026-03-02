37a8610acbab56597ec5a51ce0056cb590f18a81
Dawid <dawsze@alu.edu.gva.es>
2026-03-02 10:41:10 +0100

    guardar cambios

---
+++ src/app/Http/Controllers/Api/EventoController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ApiEventosPost;
+use App\Http\Requests\EventosPost;
+use App\Models\Especies;
+use App\Models\Eventos;
+use App\Models\Usuarios;
+use DateTime;
+use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+
+class EventoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $eventos = Eventos::select('id','nombre', 'tipo_evento', 'tipo_terreno', 'ubicacion', 'fecha', 'descripcion', 'imagen', 'pdf', 'anfitrion_id', 'estado_evento')->get();
+
+        return response()->json($eventos,202);
+    }
+
+    
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ApiEventosPost $request)
+    {
+        
+        $usuario = Usuarios::findOrFail($request->anfitrion_id);
+        $especie = Especies::findOrFail($request->id_especie);
+        $evento = Eventos::create([
+            'nombre' => $request->nombre,
+            'tipo_evento' => $request->tipo_evento,
+            'tipo_terreno' => $request->tipo_terreno,
+            'ubicacion' => $request->ubicacion,
+            'fecha' => $request->fecha,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'pdf' => $request->pdf,
+            'anfitrion_id' => $usuario->id
+        ]);
+       
+        $evento->especies()->syncWithoutDetaching([$especie->id]);
+
+        return response()->json($evento, 202);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $evento = Eventos::select('id','nombre', 'tipo_evento', 'tipo_terreno', 'ubicacion', 'fecha', 'descripcion', 'imagen', 'pdf', 'anfitrion_id', 'estado_evento')->where('id', '=' , $id)->get();
+
+        return response()->json($evento, 202);
+    }
+
+   
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $usuario = Usuarios::findOrFail($request->anfitrion_id);
+        $especie = Especies::findOrFail($request->id_especie);
+        $evento = Eventos::findOrFail($id);
+         $evento->update([
+            'nombre' => $request->nombre != "" ? $request->nombre : $evento->nombre,
+            'tipo_evento' => $request->tipo_evento != "" ? $request->tipo_evento : $evento->tipo_evento,
+            'tipo_terreno' => $request->tipo_terreno != "" ? $request->tipo_terreno : $evento->tipo_terreno,
+            'ubicacion' => $request->ubicacion != "" ? $request->ubicacion : $evento->ubicacion,
+            'fecha' => $request->fecha > new DateTime('today') ? $request->fecha : $evento->fecha,
+            'descripcion' => $request->descripcion != "" ? $request->descripcion : $evento->descripcion,
+            'imagen' => $request->imagen ?: $evento->imagen,
+            'pdf' => $request->pdf ?: $evento->pdf
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -6,6 +6,7 @@ use App\Models\Usuarios;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Api\UsuarioController as UsuarioApiController;
 
 Route::get('/', [EventosController::class, 'index'])->name('inicio');
 
@@ -17,6 +18,11 @@ Route::post('login', [UsuariosController::class, 'login'])->name('login');
 Route::get('logout', [UsuariosController::class, 'logout'])->name('logout');
 Route::post('signUp/{id}/{show}', [UsuariosController::class, 'signUp'])->name('signUp');
 Route::post('signOff/{id}/{show}', [UsuariosController::class, 'signOff'])->name('signOff');
+
+
+
+
+
 
 Route::get('/auth/google', function () {
     return Socialite::driver('google')->redirect();
